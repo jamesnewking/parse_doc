15b96fda463c4cb2048c7d46a7b1333793924bdc
James Wang <jamesnewking@gmail.com>
2018-12-18 22:48:35 -0800

    categorize words to adjectives, nouns, and verbs

--- test/doc_to_array.js
+++ test/doc_to_array.js
@@ -1,33 +1,59 @@
 //to run in node.js
-//node doc_to_array filename.txt
+//node doc_to_array inFileName.txt outFileName.txt
+const verbs = require('./verbs'); //633 verbs
+const nouns = require('./nouns'); //2332 nouns
+const adjectives = require('./adjectives'); //1099 adjectives
+
 let wordCount = {};
 var fs = require('fs');
-const fileName = process.argv[2];
+const inFileName = process.argv[2];
+const outFileName = process.argv[3];
 const regex = /\s\||[^\w]/;
 let wordCountArray = [];
+let wordsTotal = 0;
 
-//let array = fs.readFileSync(fileName).toString().split("\n");
-//let array = fs.readFileSync(fileName).toString().split(" ");
-let array = fs.readFileSync(fileName).toString().split(regex);
+function wordType(word){
+    let isType = "__";
+    if(verbs.indexOf(word)>=0){
+        isType = "verb";
+    } else if(nouns.indexOf(word)>=0){
+        isType = "noun";
+    } else if(adjectives.indexOf(word)>=0){
+        isType = "adjective";}
+    return isType;
+}
+
+//let array = fs.readFileSync(inFileName).toString().split("\n");
+//let array = fs.readFileSync(inFileName).toString().split(" ");
+let array = fs.readFileSync(inFileName).toString().split(regex);
 for(i in array) {
     let word = array[i].toLowerCase();
-    if( wordCount[word]===undefined ){
-        wordCount[word] = 1;
-    } else {
-        wordCount[word]++;
+    if(word === ""){
+        continue;
     }
-    //console.log(array[i]);
+    let tempType = wordType(word);
+    wordsTotal++;
+    if( wordCount[word] === undefined ){
+        wordCount[word] = {"count": 1, type: tempType};
+    } else {
+        wordCount[word].count++;
+    }
 }
 for (j in wordCount){
     let tempObj = {};
     tempObj[j] = wordCount[j];
     wordCountArray.push(tempObj);
 }
-wordCountArray.sort((a,b) => Object.keys(b).map(k => b[k]) - Object.keys(a).map(k => a[k]));
-for (k in wordCountArray){
-    console.log(wordCountArray[k]);
-}
+//wordCountArray.sort((a,b) => Object.keys(b).map(k => b[k]) - Object.keys(a).map(k => a[k]));
+//wordCountArray.sort( (a,b) => b.count - a.count );
+wordCountArray.sort((a,b) => Object.keys(b).map(k => b[k].count) - Object.keys(a).map(k => a[k].count));
 
-// for(k in wordCount){
-//     console.log(`${k} ${wordCount[k]}`);
-// }
+console.log(wordCountArray);
+console.log(`Total words in ${inFileName} is ${wordsTotal}`);
+console.log(`Output to ${outFileName}`);
+
+let openFile = fs.createWriteStream(outFileName);
+openFile.on('error', err => console.log(err));
+openFile.write(`Total words in ${inFileName} is ${wordsTotal}` + '\n');
+wordCountArray.forEach( v => openFile.write(JSON.stringify(v) + '\n') );
+openFile.end();
